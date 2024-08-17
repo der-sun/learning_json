c1f684711146a6b67b1d0e05fc433c78c54a9c79
AlexanderVinogradov <der_sun@bk.ru>
2024-08-17 11:55:29 +0300

    first try

---
+++ learning_json/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace learning_json
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
